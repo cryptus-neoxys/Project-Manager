818353b3cd9424bb9784e4283f1fa763380d7736
Your Name <you@example.com>
2020-09-25 21:46:02 +0530

    Added Card component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import List from "./components/List/List";
+library.add(faTrash);
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div>Board</div>
+      <div>Board
+        <List></List>
+      </div>
+      
       <div>Footer</div>
     </div>
   )
